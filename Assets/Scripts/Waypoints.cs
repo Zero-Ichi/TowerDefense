c165006c086d4d5b0bdf873acee636ae20d3553f
Ronan <r.dhersignerie@gmail.com>
2018-11-08 21:24:45 +0100

    Initial commit

---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    public static Transform[] Points;
+
+    // Awake est appelé quand l'instance de script est chargée
+    private void Awake()
+    {
+        Points = new Transform[transform.childCount];
+
+        for (int i = 0; i < Points.Length; i++)
+        {
+            Points[i] = transform.GetChild(i);
+        }
+    }
+
+
+}
